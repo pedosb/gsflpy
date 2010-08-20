e4399339899aa3c7bdb98be75ad5a4f2cb0711c3
localuser <localuser@laps21.laps.ufpa.br>
2010-08-20 12:09:23 -0300

    usando sort e prunning

--- gsflpy/Main.py
+++ gsflpy/Main.py
@@ -5,4 +5,7 @@ from ReadLattice import ReadLattice
 if __name__ == "__main__":
     read = ReadLattice()
     lattice = read.parse(sys.argv[1])
-    lattice.search_sentences()
+    sentences = lattice.search_sentences(6)
+    sentences.sort()
+    for sentence in sentences:
+       print sentence

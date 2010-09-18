64b218190c445a67cfb664ab4c95aec2a4b0a478
08080002701 <08080002701@8d5e1b68-b3dc-4992-894d-31ce13f394a2>
2010-09-18 01:58:23 +0000

    mudança de medoto __add__ para add e não sobrescrever o operador, e fazendo com que o copy não retorne o ultimo nó
    git-svn-id: https://www.laps.ufpa.br/svn/Speech/lattice@344 8d5e1b68-b3dc-4992-894d-31ce13f394a2

--- gsflpy/Sentence.py
+++ gsflpy/Sentence.py
@@ -4,26 +4,56 @@ class Sentence():
       self.links = []
       self.nodes = []
       self.last_node = None
+      self._score = None
       self.ready = False
       if link:
-	 self += link
+	 self.add(link)
 
-   def __add__(self, link):
+   def add(self, link):
       if isinstance(link, Link):
+	 if not self._score:
+	    self._score = 0
 	 self.links.append(link)
+	 if link.d:
+	    for segment in link.d:
+	       self._score += segment.score
+	 if not self.last_node:
+	    self.nodes.append(link.s)
 	 self.nodes.append(link.e)
 	 self.last_node = link.e
-	 return self
       else:
 	 #TODO: Create an exception
 	 pass
 
    def __str__(self):
-      #TODO: Make return the phase formed by the nodes
-      return 'working to show it'
+      string = ''
+      for node in self.nodes:
+	 string += ' ' + node.w
+      string += '  ' + str(self._score)
+      return string
+
+   def __cmp__(self, other):
+      """
+      Compare based on the score of the entire sentence.
+      """
+      if self._score > other._score:
+	 return -1
+      elif self._score < other._score:
+	 return 1
+      else:
+	 return 0
 
    def copy(self):
+      """
+      Create a copy of the sentence.
+      Warnning: It does not copy the last node
+      """
       sentence = Sentence()
-      for link in self.links:
-	 sentence += link
+      for link in self.links[0:len(self.links)-1]:
+	 sentence.add(link)
       return sentence
+
+
+
+
+

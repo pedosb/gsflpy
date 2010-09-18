dabb704fd9e512c2c48b5038ce9fbda49ffecf62
08080002701 <08080002701@8d5e1b68-b3dc-4992-894d-31ce13f394a2>
2010-09-18 02:01:19 +0000

    script para converter lista do nelson para entrada do gsflpy -L
    git-svn-id: https://www.laps.ufpa.br/svn/Speech/lattice@380 8d5e1b68-b3dc-4992-894d-31ce13f394a2

---
+++ gsflpy/list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+if __name__=="__main__":
+   f = open(sys.argv[2], 'w')
+   for line in open(sys.argv[1]):
+      words = line.split()
+      f.write('/ubuntu/home/pedro/Downloads/digits16k/lat/' + words[0])
+      f.write(' ' + str(len(words)+4))
+      f.write(' !NULL SENT-START')
+      for word in words[1:len(words)]:
+	 f.write(' ' + word)
+      f.write(' SENT-END !NULL\n')
+   f.flush()
+   f.close()

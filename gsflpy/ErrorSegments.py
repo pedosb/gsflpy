68dab2e0e6ed8da9c6ab3afb6aad195bdc411246
Pedro Batista <edokdz@gmail.com>
2010-10-17 21:23:48 -0400

    Make it receive options about confusion.
    It now receive as option the phonemes to consider as confusion. An
    option to consider negative margins as yes.

--- gsflpy/ErrorSegments.py
+++ gsflpy/ErrorSegments.py
@@ -1,14 +1,30 @@
 import matplotlib.pyplot as plt
+import sys
 from ErrorSegment import ErrorSegment
 from Arff import Arff
 import util
+
 #TODO This is not a good name
 class ErrorSegments():
-   def __init__(self,error_segments):
+   def __init__(self,
+	 error_segments,
+	 states_confusion,
+	 negative,
+	 confusion):
+      self.states_confusion = states_confusion
+      self.negative = negative
+      self.confusion = confusion
+      if self.negative and not self.confusion:
+	 print ('ERROR: Cannot have negative margin with False confusion.' +
+	       '(Not tested yet).')
+	 sys.exit(-1)
       self.error_segments = error_segments
       self.arff = Arff()
 
    def most_different(self):
+      if self.negative:
+	 self.states_confusion = (self.states_confusion[1],
+	       self.states_confusion[0])
       new_error_segments = []
       index_error_segment = 0
       for error_segment in self.error_segments:
@@ -32,9 +48,10 @@ class ErrorSegments():
 #			[index_segment].state
 	       index_segment += 1
 
-	    margins.sort(reverse=False)
+	    margins.sort(reverse=not self.negative)
 	    for margin in margins:
-	       if margin[0] > 0:
+	       if (margin[0] > 0 and self.negative) or (margin[0] < 0 and
+		     not self.negative):
 		  break
 	       #if the states of the max margin is the same continue
 	       if segments[error_segment.correct_index[index_segments]]\
@@ -60,15 +77,17 @@ class ErrorSegments():
 
 	       for samples_key in samples:
 		  samples[samples_key] = biggest / samples[samples_key]
-	       samples['confusao'] = 'sim'
+	       samples['confusion'] = str(self.confusion)
 
 	       new_error_segment = ErrorSegment(new_segments,
 			error_segment.correct_index[index_segments],
 			error_segment.start_time[index_segments]+margin[1],
 			error_segment.file_name[index_segments])
 
-	       if (new_error_segment.correct_states == 'v[4]' and
-		     new_error_segment.recognized_states == 'ow1[4]'):
+	       if (new_error_segment.correct_states == self.states_confusion[1] and
+		     new_error_segment.recognized_states ==
+		     self.states_confusion[0]):
+		  samples['filename'] = error_segment.file_name[index_segments]
 		  self.arff.add(samples)
 
 		  util.add_error_segment(new_error_segments,
@@ -82,6 +101,6 @@ class ErrorSegments():
       for error_segment in new_error_segments:
 	 if len(error_segment.segments):# > 20:
 	    print str(error_segment) + ' qtd ' + str(len(error_segment.segments))
-	    for file_name in error_segment.file_name:
-	       print file_name
+#	    for file_name in error_segment.file_name:
+#	       print file_name
       return new_error_segments, self.arff
